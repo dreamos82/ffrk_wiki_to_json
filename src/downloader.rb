7d86fd2ee5ff526dea8c2c7d8188366c1863ef0a
Ivan Gualandri <ivan.gualandri@gmail.com>
2018-11-02 11:10:24 +0000

    Add argument parsing, refactor code, add output option to specify output filename

--- src/downloader.rb
+++ src/downloader.rb
@@ -1,6 +1,7 @@
 require 'net/http'
 require 'nokogiri'
 require 'json'
+require 'optparse'
 
 $WIKI_PREFIX= 'https://ffrkstrategy.gamematome.jp'
 def get_details(url)
@@ -19,8 +20,44 @@ def get_details(url)
 end
 
 def parse_table(sbtable)
+	sbitems = []
+	sbtable.search('tr').each do |tr|
+		cells = tr.search('th, td')
+		elements = cells[1].child
+	
+		if(!elements.attribute('href').nil?)
+			cur_item = get_details(elements.attribute('href'))
+			cur_item["name"] = cells[1].text.strip
+			cur_item["hero"] = cells[2].text.strip
+			cur_item["realm"] = cells[3].text.strip
+			sbitems.push(cur_item)
+		end
+	end
+	return sbitems
 end
 
+def save_file(filename, json_output) 
+	f = File.new(filename, 'w')
+	f.write(json_output)
+	f.close
+end
+
+options = {}
+optparse = OptionParser.new do|opts|
+	opts.banner = "Usage: downloader.rb [options]"
+	opts.on( '-o', '--output FILENAME', 'Save output to file FILENAME') do |filename|
+		options[:filename] = filename
+	end
+	opts.on( '-h', '--help', 'Display this screen' ) do
+   		 puts opts
+    		exit
+  	end
+end
+if(options[:filename]) 
+	filename = options[:filename]
+else
+	filename = 'output.json'
+end
 uri = URI.parse('https://ffrkstrategy.gamematome.jp/game/951/wiki/Soul%20Break%20List_Default%20Soul%20Break')
 http = Net::HTTP.new(uri.host, uri.port)
 http.use_ssl = true
@@ -30,23 +67,7 @@ document = Nokogiri::HTML(output.body)
 
 sbtable = document.at('#content_block_5')
 
-sbitems = []
-
-sbtable.search('tr').each do |tr|
-	cells = tr.search('th, td')
-	elements = cells[1].child
-	
-	if(!elements.attribute('href').nil?)
-		cur_item = get_details(elements.attribute('href'))
-		cur_item["name"] = cells[1].text.strip
-		cur_item["hero"] = cells[2].text.strip
-		cur_item["realm"] = cells[3].text.strip
-		sbitems.push(cur_item)
-	end
-end
+sbitems = parse_table(sbtable)
 
 json_output = JSON.pretty_generate(sbitems)
-
-f = File.new('output.json', 'w')
-f.write(json_output)
-f.close
+save_file(filename, json_output)
